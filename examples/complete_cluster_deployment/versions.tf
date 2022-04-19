c943b3b3c8d1a9011d740fca72e376e91f40d391
eminchen <eminchen@cisco.com>
2022-04-19 16:14:52 -0400

    Update versions.tf

--- examples/complete_cluster_deployment/versions.tf
+++ examples/complete_cluster_deployment/versions.tf
@@ -1,10 +1,16 @@
 terraform {
-  required_version = ">=1.1.0"
-
-  required_providers {
-    intersight = {
-      source  = "CiscoDevNet/intersight"
-      version = "=1.0.19"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "Toronto_DCLAB"
+    workspaces {
+      name = "terraform-intersight-iks"
     }
   }
+  required_providers {
+    intersight = {
+      source = "CiscoDevNet/intersight"
+      # version = "1.0.12"
+    }
+  }
+  # experiments = [module_variable_optional_attrs]
 }
