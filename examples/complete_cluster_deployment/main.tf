b2011d815750d46ef660fa44ed91702f494f21de
eminchen <eminchen@cisco.com>
2022-05-09 11:54:34 -0400

    Update main.tf

--- examples/complete_cluster_deployment/main.tf
+++ examples/complete_cluster_deployment/main.tf
@@ -75,7 +75,7 @@ module "iks_cluster" {
   versionPolicy = {
     useExisting    = false
     policyName     = "1.21"
-    iksVersionName = "1.21.10-iks.0"
+    iksVersionName = "v1.21.11"
   }
 
   # Trusted Registry Policy (To create new change "use_existing" to 'false' and set "create_new' to 'true' uncomment variables and modify them to meet your needs.)
